40a0df702822471ee5162b46fe6549d06c71fc1d
Yuan Liao <liaoyuan@gmail.com>
2021-06-08 17:13:25 -0700

    Ignore comments and white space for ebuild comparison

---
+++ tests/scripts/compare-ebuilds.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Compare the ebuilds pointed to by paths stored in environment variables
+# EXPECTED and ACTUAL for equivalence.
+
+# Prepare the ebuild passed in via standard input for comparison, and print the
+# preprocessing result to standard output.
+#
+# Preprocessing steps include:
+# 1. Removal of lines that only contain a comment
+preprocess_ebuild() {
+    grep -v '^#'
+}
+
+# Compare EXPECTED and ACTUAL, and print the difference to standard output.
+#
+# The rules for comparison are:
+# - Line ordering matters
+# - Differences in white space are ignored
+# - Preprocessing done by the 'preprocess_ebuild' function is applied
+compare_ebuilds() {
+    diff -w --color=always \
+        <(preprocess_ebuild < "${EXPECTED}") \
+        <(preprocess_ebuild < "${ACTUAL}")
+}
--- tests/scripts/single-ebuild-test.sh
+++ tests/scripts/single-ebuild-test.sh
@@ -3,13 +3,14 @@
 # Create an ebuild for MAVEN_ARTS, and test if the ebuild java-ebuilder created
 # for it is equivalent to the expected output.
 
+source "${SRC_ROOT}/tests/scripts/compare-ebuilds.sh"
 source "${SRC_ROOT}/tests/scripts/movl-mock.sh"
 
 run_single_ebuild_test() {
-    expected="${EXPECTED_EBUILDS_DIR}/${EBUILD_PATH}"
-    actual="${MAVEN_OVERLAY_DIR}/${EBUILD_PATH}"
+    EXPECTED="${EXPECTED_EBUILDS_DIR}/${EBUILD_PATH}"
+    ACTUAL="${MAVEN_OVERLAY_DIR}/${EBUILD_PATH}"
 
     movl_mock build > /dev/null
-    diff --color=always "${expected}" "${actual}"
+    compare_ebuilds
     movl_mock clean > /dev/null
 }

1394ecc354b19426db33870a9aecb8fd0972accb
Yuan Liao <liaoyuan@gmail.com>
2021-06-13 11:07:23 -0700

    Add a system testing framework based on Bash scripts
    Ignore comments and white space for ebuild comparison
    
    Unconditionally redirect all outputs of make to /dev/null
    
    Exit ebuilds test script with status indicating comparison results
    
    Double quote variables for path extensions where it is forgotten
    
    Print message to indicate overall test result
    
    Use unified diff format to show ebuild discrepancies
    
    Do not generate digests for ebuilds during test
    
    The digests are only useful when the ebuilds are actually put into an
    overlay.  For the purpose of comparing ebuilds for equivalence, digests
    are not needed.
    
    Add support for multiple Maven artifacts in single test case
    
    Add support for running standalone test cases
    
    ebuild-tests.sh: Add help message
    
    Preprocess ebuilds generated during tests in place
    
    This will let the file names of any mismatched ebuilds show up in diff,
    making it easier to determine the exact path to problematic ebuilds.
    
    Allow overriding REPOS variable for individual test cases
    
    This would make it possible to use additional overlays in a single test
    case.
    
    Declare ephemeral environment variables in test execution as local
    
    This can help avoid surprises caused by outdated environment variable
    values.
    
    Enable test cases to use local variables without polluting environment
    
    ebuild-tests.sh: Clarify type of variables in help text
    
    ebuild-tests.sh: Add notes for REPOS to help text
    
    ebuild-tests.sh: Report test case file name instead of MAVEN_ARTS
    
    This would make it easier to pinpoint the file name of failing test
    cases.  If only MAVEN_ARTS is reported, one would need to search for its
    occurrences in the test case files, which would add an extra step.
    
    ebuild-tests.sh: Clean up comment explaining test case format
    
    The help message added later is already explaining this.
    
    ebuild-tests.sh: Add a dedicated function for command parsing
    
    Add an option to enable verbose test output
    
    ebuild-tests.sh: Protect option flags from being sourced
    
    ebuild-tests.sh: Update script output strings
    
    single-ebuild-test.sh: Improve style for local variable declaration

---
+++ tests/scripts/single-ebuild-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Create ebuild(s) for MAVEN_ARTS, and test if the ebuild(s) java-ebuilder
+# created for it is equivalent to the expected output.
+
+source "${SRC_ROOT}/tests/scripts/compare-ebuilds.sh"
+source "${SRC_ROOT}/tests/scripts/movl-mock.sh"
+
+run_single_ebuild_test() {
+    local portage_repos
+    portage_repos=$(portageq get_repo_path / $(portageq get_repos /))
+    # Change new lines to spaces
+    portage_repos=$(tr '\n' ' ' <<< "${portage_repos}")
+    local REPOS="${portage_repos} ${REPOS}"
+    movl_mock stage2
+    for ebuild in ${EBUILD_PATHS}; do
+        # Remove leading and trailing white space
+        local ebuild=$(sed \
+            -e 's/^[[:space:]]*//' \
+            -e 's/[[:space:]]*$//' <<< "${ebuild}")
+        local EXPECTED="${EXPECTED_EBUILDS_DIR}/${ebuild}"
+        local ACTUAL="${MAVEN_OVERLAY_DIR}/${ebuild}"
+        compare_ebuilds
+    done
+    local cmp_result=$?
+    movl_mock clean
+    return ${cmp_result}
+}

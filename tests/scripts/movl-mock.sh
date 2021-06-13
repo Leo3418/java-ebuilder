ba8c680b52fcd52801e3a42a1e3dee64e8e0f939
Yuan Liao <liaoyuan@gmail.com>
2021-06-13 11:07:23 -0700

    Add a system testing framework based on Bash scripts
    Ignore comments and white space for ebuild comparison
    
    Unconditionally redirect all outputs of make to /dev/null
    
    Exit ebuilds test script with status indicating comparison results
    
    Double quote variables for path extensions where it is forgotten
    
    Print message to indicate overall test result
    
    Use unified diff format to show ebuild discrepancies
    
    Do not generate digests for ebuilds during test
    
    The digests are only useful when the ebuilds are actually put into an
    overlay.  For the purpose of comparing ebuilds for equivalence, digests
    are not needed.
    
    Add support for multiple Maven artifacts in single test case
    
    Add support for running standalone test cases
    
    ebuild-tests.sh: Add help message
    
    Preprocess ebuilds generated during tests in place
    
    This will let the file names of any mismatched ebuilds show up in diff,
    making it easier to determine the exact path to problematic ebuilds.
    
    Allow overriding REPOS variable for individual test cases
    
    This would make it possible to use additional overlays in a single test
    case.
    
    Declare ephemeral environment variables in test execution as local
    
    This can help avoid surprises caused by outdated environment variable
    values.
    
    Enable test cases to use local variables without polluting environment
    
    ebuild-tests.sh: Clarify type of variables in help text
    
    ebuild-tests.sh: Add notes for REPOS to help text
    
    ebuild-tests.sh: Report test case file name instead of MAVEN_ARTS
    
    This would make it easier to pinpoint the file name of failing test
    cases.  If only MAVEN_ARTS is reported, one would need to search for its
    occurrences in the test case files, which would add an extra step.
    
    ebuild-tests.sh: Clean up comment explaining test case format
    
    The help message added later is already explaining this.
    
    ebuild-tests.sh: Add a dedicated function for command parsing
    
    Add an option to enable verbose test output
    
    ebuild-tests.sh: Protect option flags from being sourced
    
    ebuild-tests.sh: Update script output strings
    
    single-ebuild-test.sh: Improve style for local variable declaration
    
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

---
+++ tests/scripts/movl-mock.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Mock the behavior of the 'movl' script installed with java-ebuilder.
+
+# The classpath used to run java-ebuilder
+: ${CLASSPATH:="${SRC_ROOT}/target/classes"}
+
+# The command to run java-ebuilder
+JAVA_EBUILDER="java -classpath ${CLASSPATH} org.gentoo.java.ebuilder.Main"
+
+# The look-up table's path
+LUTFILE="${SRC_ROOT}/scripts/resources/resources/LUT"
+
+# The directory that contains the helper scripts
+HELPERS_ROOT="${SRC_ROOT}/scripts/bin"
+
+# The directory that contains Makefiles that invoke java-ebuilder
+MAKEFILE_DIR="${SRC_ROOT}/scripts/resources/Makefiles"
+
+# The Makefile that serves as the entry point to all Makefiles in MAKEFILE_DIR
+MAKEFILE="${MAKEFILE_DIR}/Makefile"
+
+# Mock the 'movl' helper script.
+movl_mock() {
+    if ! "${VERBOSE}"; then
+        local output_redirect=" > /dev/null 2>&1"
+    fi
+    # Assemble a command like
+    # env NAME="${VALUE}" make -f "${MAKEFILE}" "$@" "${output_redirect}"
+    # to copy environment variables in the current shell into the new
+    # environment for 'make' invocation
+    eval "env \
+        CACHEDIR=\"${CACHEDIR}\" \
+        CONFIG=\"${CONFIG}\" \
+        HELPERS_ROOT=\"${HELPERS_ROOT}\" \
+        JAVA_EBUILDER=\"${JAVA_EBUILDER}\" \
+        LUTFILE=\"${LUTFILE}\" \
+        MAKEFILE_DIR=\"${MAKEFILE_DIR}\" \
+        MAVEN_ARTS=\"${MAVEN_ARTS}\" \
+        MAVEN_OVERLAY_DIR=\"${MAVEN_OVERLAY_DIR}\" \
+        POMDIR=\"${POMDIR}\" \
+        REPOS=\"${REPOS}\" \
+        STAGE1_DIR=\"${STAGE1_DIR}\" \
+        STAGE2_MAKEFILE=\"${STAGE2_MAKEFILE}\"" \
+        make -f "${MAKEFILE}" "$@" "${output_redirect}"
+}
---
+++ tests/scripts/single-ebuild-test.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Create ebuild(s) for MAVEN_ARTS, and test if the ebuild(s) java-ebuilder
+# created for it is equivalent to the expected output.
+
+source "${SRC_ROOT}/tests/scripts/compare-ebuilds.sh"
+source "${SRC_ROOT}/tests/scripts/movl-mock.sh"
+
+# Initialize the REPOS environment variable with the path to the Gentoo
+# repository installed on the system, and any custom repositories used for the
+# current test case specified by the TEST_REPOS environment variable.
+init_repos() {
+    local portage_repos
+    # Use only the Gentoo repository to avoid discrepancies in generated
+    # ebuilds caused by additional custom repositories installed on the system
+    portage_repos="$(portageq get_repo_path / gentoo)"
+    # Change new lines to spaces
+    portage_repos=$(tr '\n' ' ' <<< "${portage_repos}")
+
+    local test_repos_array
+    test_repos_array=()
+    if [[ -n "${TEST_REPOS}" ]]; then
+        for repo in ${TEST_REPOS}; do
+            test_repos_array+=( "${TEST_REPOS_DIR}/${repo}" )
+        done
+    fi
+
+    REPOS="${portage_repos} ${test_repos_array[*]}"
+}
+
+run_single_ebuild_test() {
+    init_repos
+
+    movl_mock stage2
+    for ebuild in ${EBUILD_PATHS}; do
+        # Remove leading and trailing white space
+        local ebuild=$(sed \
+            -e 's/^[[:space:]]*//' \
+            -e 's/[[:space:]]*$//' <<< "${ebuild}")
+        local search_path="${EXPECTED_EBUILDS_ROOT}/${EXPECTED_EBUILDS_SUBDIR}"
+        local EXPECTED="${search_path}/${ebuild}"
+        local ACTUAL="${MAVEN_OVERLAY_DIR}/${ebuild}"
+        compare_ebuilds
+    done
+    local cmp_result=$?
+    movl_mock clean
+    return ${cmp_result}
+}
